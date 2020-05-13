e0aab5ce9b4d1b9c330e45cccaf04fa49dc9e3b0
jam <nevvjann@gmail.com>
2020-05-13 14:53:30 +0900

    Add: User views

--- rabbit-backend/user/views.py
+++ rabbit-backend/user/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model, authenticate, login
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+
+class UserViewSet(ModelViewSet):
+    model = User
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def get_object(self):
+        pk = self.kwargs.get('pk', None)
+        return User.objects.get(pk=pk)
+
+
+class Login(APIView):
+    def post(self, request, *args, **kwargs):
+        data = {
+            'username': self.request.data.get('username', None),
+            'password': self.request.data.get('password', None),
+        }
+        user = authenticate(request, **data)
+        token, _ = Token.objects.get_or_create(user=user)
+        login(request, user)
+        return Response({"auth_token": token.key}, status=status.HTTP_200_OK)
